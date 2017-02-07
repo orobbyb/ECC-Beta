5cf28583f43ee1c28a2e3aaae7a29a8d6b6dc743
tickct <seangraysota@gmail.com>
2017-02-07 14:41:18 -0600

    route logout back to login

--- ecc-cms/config/routes.rb
+++ ecc-cms/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-  
+  get 'logout' => 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
