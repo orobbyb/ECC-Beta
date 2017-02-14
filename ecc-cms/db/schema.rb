148582b5fae964a76b1e61642a2596059214862e
Shahzil Sheikh <shazy792@hotmail.com>
2017-02-14 01:33:51 -0600

    New Migration for Documents model to include more detials.

--- ecc-cms/db/schema.rb
+++ ecc-cms/db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205171335) do
+ActiveRecord::Schema.define(version: 20170214073129) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.text     "description"
+    t.text     "disclaimer"
+    t.date     "date"
+    t.string   "organization_url"
+    t.string   "source_value"
+    t.string   "source_url"
+    t.text     "taxonomy"
+    t.string   "version"
   end
 
   create_table "users", force: :cascade do |t|
