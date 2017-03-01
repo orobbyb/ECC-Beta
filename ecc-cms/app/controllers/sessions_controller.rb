c8efbb89c1935bd806d6049d5135306388c73f23
Shahzil Sheikh <shazy792@hotmail.com>
2017-03-01 00:46:54 -0600

    Some comments and Trix in the Gemlock File

--- ecc-cms/app/controllers/sessions_controller.rb
+++ ecc-cms/app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # TODO: Create logic for authentication to use with elastic search
     user = User.find_by(name: params[:name])
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
