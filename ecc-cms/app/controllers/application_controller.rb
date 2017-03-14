ec8fbfc34029733083b11ca664b3125c43ca6283
Shahzil Sheikh <shazy792@hotmail.com>
2017-03-14 14:29:28 -0500

    Intentionally Breaking Authentication

--- ecc-cms/app/controllers/application_controller.rb
+++ ecc-cms/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authorize # needed to allow for site to work
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
     # ...
 
