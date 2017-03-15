61a86976ab23d679c3f1b587220ab9fad075cfa2
Shahzil Sheikh <shazy792@hotmail.com>
2017-03-14 20:57:21 -0500

    Fixing search controller

--- ecc-cms/app/controllers/search_controller.rb
+++ ecc-cms/app/controllers/search_controller.rb
@@ -2,9 +2,10 @@ class SearchController < ApplicationController
     # Use params[:query] to search the DB - Over here
     # Use result from DB to redirect the user to the correct location
     def search
-    if params[:q].nil?
-        @articles = []
-    else
-        @articles = Article.search params[:q]
+        if params[:q].nil?
+            @articles = []
+        else
+            @articles = Article.search params[:q]
+        end
     end
 end
