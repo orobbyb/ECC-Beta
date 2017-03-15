693cb4aa8d172de3bd60aba31caa99612751b1bc
Shahzil Sheikh <shazy792@hotmail.com>
2017-03-14 20:49:11 -0500

    Testing Search functionality

--- ecc-cms/app/controllers/search_controller.rb
+++ ecc-cms/app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
+    # Use params[:query] to search the DB - Over here
+    # Use result from DB to redirect the user to the correct location
     def search
-        # Use params[:query] to search the DB - Over here
-        # Use result from DB to redirect the user to the correct location
+    if params[:q].nil?
+        @articles = []
+    else
+        @articles = Article.search params[:q]
     end
 end
