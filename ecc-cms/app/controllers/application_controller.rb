4901526b600d265f89c348d29629281d2d45cb6d
tickct <seangraysota@gmail.com>
2017-03-21 14:53:13 -0500

    fixed authentication

--- ecc-cms/app/controllers/application_controller.rb
+++ ecc-cms/app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
-  # before_action :authorize # needed to allow for site to work
-  # protect_from_forgery with: :exception
+   before_action :authorize # needed to allow for site to work
+   protect_from_forgery with: :exception
 
     # ...
 
   protected
     def authorize
-      unless User.find_by(id: session[:user_id])
+      begin
+        User.search(query:{match:{id: session[:user_id]}})
+      rescue
         redirect_to login_url, notice: "Please log in"
       end
     end
